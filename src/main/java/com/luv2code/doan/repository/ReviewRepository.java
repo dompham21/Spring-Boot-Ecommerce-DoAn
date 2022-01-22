3d2d9cf2f34d0a6e8bdaa28a7216801c0586be2a
dompham21 <63381043+dompham21@users.noreply.github.com>
2022-01-22 10:57:48 +0700

    v1.0.0 Them chuc nang review san pham

---
+++ src/main/java/com/luv2code/doan/repository/ReviewRepository.java
@@ -0,0 +1,18 @@
+package com.luv2code.doan.repository;
+
+import com.luv2code.doan.entity.Product;
+import com.luv2code.doan.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer>  {
+    @Query("SELECT r FROM Review r WHERE r.product.id = :id ORDER BY r.date DESC")
+    public Page<Review> findReviewByProduct(Integer id, Pageable pageable);
+
+    @Query("SELECT count(r.id) FROM Review r WHERE r.product.id = :id AND r.vote = :starNum")
+    public Integer countStarNumByProduct(Integer id, Integer starNum);
+}
